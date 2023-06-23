2d6bfeb0e4973c12ad9e369bdded6ba4208289de
Shahadat Hossain <shahadat3669@gmail.com>
2023-06-23 13:08:14 +0600

    Add props for Input component

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,6 +1,26 @@
 'use client';
 
-const Input = () => {
+import { UseFormRegister, FieldValues, FieldErrors } from 'react-hook-form';
+
+interface InputProps {
+  id: string;
+  label: string;
+  type?: boolean;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+  disabled?: boolean;
+}
+
+const Input: React.FC<InputProps> = ({
+  id,
+  label,
+  type,
+  required,
+  register,
+  errors,
+  disabled,
+}) => {
   return <div>Input</div>;
 };
 
