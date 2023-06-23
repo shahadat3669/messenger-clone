0290b12b3175a526e6ce64df97f6fac3fbcf8c61
Shahadat Hossain <shahadat3669@gmail.com>
2023-06-23 12:22:27 +0600

    Add variants state and toggleVariant method to toggle login nad register form switch

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,6 +1,19 @@
 'use client';
 
+import { useCallback, useState } from 'react';
+
+type variant = 'LOGIN' | 'RESISTER';
+
 const AuthForm = () => {
+  const [variant, setVariant] = useState<variant>('LOGIN');
+  const toggleVariant = useCallback(() => {
+    if (variant === 'LOGIN') {
+      setVariant('RESISTER');
+    } else {
+      setVariant('LOGIN');
+    }
+  }, [variant]);
+
   return <div>AuthForm</div>;
 };
 
