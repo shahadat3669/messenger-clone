e07fd126bab2df8c018222a92a3f3a755f0c59bb
Shahadat Hossain <shahadat3669@gmail.com>
2023-06-23 12:41:45 +0600

    Add AuthForm form with necessary setup structure for react form hook

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import { useCallback, useState } from 'react';
+import { useForm, FieldValues, SubmitHandler } from 'react-hook-form';
 
 type variant = 'LOGIN' | 'RESISTER';
 
 const AuthForm = () => {
   const [variant, setVariant] = useState<variant>('LOGIN');
+  const [isLoading, setIsLoading] = useState(false);
   const toggleVariant = useCallback(() => {
     if (variant === 'LOGIN') {
       setVariant('RESISTER');
@@ -14,7 +16,46 @@ const AuthForm = () => {
     }
   }, [variant]);
 
-  return <div>AuthForm</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    if (variant === 'RESISTER') {
+      // resister user
+    }
+
+    if (variant === 'LOGIN') {
+      // login user
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+    // using NextAuth social sign in
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form
+          className="space-y-6"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          input components
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AuthForm;
