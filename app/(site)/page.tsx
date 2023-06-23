923066fdf1dfb02d36711d33af6b0bba7f3bf34d
Shahadat Hossain <shahadat3669@gmail.com>
2023-06-23 12:19:28 +0600

    Add authentication page title and logo

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,8 +2,19 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hi there</h1>
-    </main>
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image
+          alt="Messenger Logo"
+          height={48}
+          width={48}
+          className="mx-auto w-auto"
+          src="/images/logo.png"
+        />
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+    </div>
   );
 }
