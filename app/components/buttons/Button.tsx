17a3721e6dec185c93e8e761d216ff80cfa4c3f6
Shahadat Hossain <shahadat3669@gmail.com>
2023-06-24 22:29:31 +0600

    Add Button component

---
+++ app/components/buttons/Button.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import clsx from 'clsx';
+
+interface ButtonProps {
+  type?: 'button' | 'submit' | 'reset' | undefined;
+  fullWidth?: boolean;
+  children?: React.ReactNode;
+  onClick?: () => void;
+  secondary?: boolean;
+  danger?: boolean;
+  disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  type,
+  fullWidth,
+  children,
+  onClick,
+  secondary,
+  danger,
+  disabled,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
